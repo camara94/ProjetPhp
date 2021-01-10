14f3853f1b2c33dc26ff40a7a8010ddad627ed0a
Laby Damaro Camara <ldamaro98@gmail.com>
2021-01-10 19:11:28 +0100

    Connexion a la base de donnees

---
+++ Connexion.php
@@ -0,0 +1,15 @@
+<?php
+	
+	function Connexion($host = 'localhost', $user = 'root', $password = '', $database = 'projetPhp')
+	{
+		
+		try {
+			$db = new PDO('mysql:host='.$host.';dbname='.$database.';charset=UTF8', $user, $password);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $db;
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+	}
+
+?>
